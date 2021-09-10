2c8f7e5347bd91ae3ee824307fcca27afc60beef
Bruno Tardin <brunotardin20@gmail.com>
2021-09-09 22:32:01 -0300

    Created Author service

--- src/main/java/com/tardin/bookstoremanager/author/service/AuthorService.java
+++ src/main/java/com/tardin/bookstoremanager/author/service/AuthorService.java
@@ -1,5 +1,7 @@
 package com.tardin.bookstoremanager.author.service;
 
+import com.tardin.bookstoremanager.author.dto.AuthorDTO;
+import com.tardin.bookstoremanager.author.exception.AuthorAlreadyExistsException;
 import com.tardin.bookstoremanager.author.mapper.AuthorMapper;
 import com.tardin.bookstoremanager.author.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,5 +17,18 @@ public class AuthorService {
     @Autowired
     public AuthorService(AuthorRepository authorRepository) {
         this.authorRepository = authorRepository;
+    }
+
+    public AuthorDTO create(AuthorDTO authorDTO){
+        verifyIfExists(authorDTO.getName());
+
+        var authorToCreate = authorMapper.toModel(authorDTO);
+        var createdAuthor = authorRepository.save(authorToCreate);
+        return authorMapper.toDTO(createdAuthor);
+    }
+
+    private void verifyIfExists(String authorName) {
+        authorRepository.findByName(authorName)
+                .ifPresent(author -> { throw new AuthorAlreadyExistsException(authorName); });
     }
 }
