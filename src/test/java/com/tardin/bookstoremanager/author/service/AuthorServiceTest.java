d1886ddff32740a7a12ead855928101d8a1adf5c
Bruno Tardin <brunotardin20@gmail.com>
2021-09-09 22:32:17 -0300

    Tests Author service

--- src/test/java/com/tardin/bookstoremanager/author/service/AuthorServiceTest.java
+++ src/test/java/com/tardin/bookstoremanager/author/service/AuthorServiceTest.java
@@ -1,17 +1,26 @@
 package com.tardin.bookstoremanager.author.service;
 
 import com.tardin.bookstoremanager.author.builder.AuthorDTOBuilder;
-import com.tardin.bookstoremanager.author.dto.AuthorDTO;
+import com.tardin.bookstoremanager.author.exception.AuthorAlreadyExistsException;
 import com.tardin.bookstoremanager.author.mapper.AuthorMapper;
 import com.tardin.bookstoremanager.author.repository.AuthorRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
-public class AuthorServiceTest {
+class AuthorServiceTest {
 
     private final AuthorMapper authorMapper = AuthorMapper.INSTANCE;
 
@@ -27,5 +36,31 @@ public class AuthorServiceTest {
     void setUp() {
         authorDTOBuilder = AuthorDTOBuilder.builder().build();
         var authorDTO = authorDTOBuilder.buildAuthorDTO();
+    }
+
+    @Test
+    void whenNewAuthorIsInformedThenItShouldBeCreated() {
+        // given
+        var expectedAuthorToCreateDTO = authorDTOBuilder.buildAuthorDTO();
+        var expectedCreatedAuthor = authorMapper.toModel(expectedAuthorToCreateDTO);
+
+        // when
+        when(authorRepository.save(expectedCreatedAuthor)).thenReturn(expectedCreatedAuthor);
+        when(authorRepository.findByName((expectedAuthorToCreateDTO.getName()))).thenReturn(Optional.empty());
+
+        var createdAuthorDTO = authorService.create(expectedAuthorToCreateDTO);
+
+        // then
+        assertThat(createdAuthorDTO, is(equalTo(expectedAuthorToCreateDTO)));
+    }
+
+    @Test
+    void whenExistingAuthorIsInformedThenAnExceptionShouldBeThrown() {
+        var expectedAuthorToCreateDTO = authorDTOBuilder.buildAuthorDTO();
+        var expectedCreatedAuthor = authorMapper.toModel(expectedAuthorToCreateDTO);
+
+        when(authorRepository.findByName((expectedAuthorToCreateDTO.getName()))).thenReturn(Optional.of(expectedCreatedAuthor));
+
+        assertThrows(AuthorAlreadyExistsException.class, () -> authorService.create(expectedAuthorToCreateDTO));
     }
 }
