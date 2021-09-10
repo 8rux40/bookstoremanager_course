11fef260a3b11ee8f4d21ca686b7ee33f2d15e01
Bruno Tardin <brunotardin20@gmail.com>
2021-09-09 22:29:43 -0300

    Created custom exception

---
+++ src/main/java/com/tardin/bookstoremanager/author/exception/AuthorAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package com.tardin.bookstoremanager.author.exception;
+
+import javax.persistence.EntityExistsException;
+
+public class AuthorAlreadyExistsException extends EntityExistsException {
+
+    public AuthorAlreadyExistsException(String name) {
+        super(String.format("Author with name %s already exists!", name));
+    }
+}
