161f79bbc019c994abdbad4461ec1265ba6662eb
Bruno Tardin <brunotardin20@gmail.com>
2021-12-08 13:49:19 -0300

    Created User DTO class

---
+++ src/main/java/com/tardin/bookstoremanager/users/dto/UserDTO.java
@@ -0,0 +1,49 @@
+package com.tardin.bookstoremanager.users.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.tardin.bookstoremanager.users.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Long id;
+
+    @NotNull
+    @NotEmpty
+    @Size(max = 255)
+    private String name;
+
+    @NotNull
+    @Max(value = 130)
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Gender gender;
+
+    @Email
+    @NotNull
+    @NotEmpty
+    private String email;
+
+    @NotNull
+    @NotEmpty
+    private String username;
+
+    @NotNull
+    @NotEmpty
+    private String password;
+
+    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyyy")
+    private LocalDate birthDate;
+}
